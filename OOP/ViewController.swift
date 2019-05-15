84cd1653c8fcb36b20d4dfeaf476f50d8915ed17
Ernest <ernest.aminata@gmail.com>
2019-05-15 08:14:13 +0700

    Initial Commit

---
+++ OOP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OOP
+//
+//  Created by ernest aminata on 15/05/19.
+//  Copyright © 2019 ernest aminata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
